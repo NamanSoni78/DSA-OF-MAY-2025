4f3ad506983ceb5bb02ca7511ca6d4ed02864e69
LND78 <Namansoni78@outlook.com>
2025-05-13 11:15:28 +0530

    ad

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,21 @@
-```cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int nums = [2,4,7,8];
+        int target = 11;
+        int ArrSize = sizeof(nums) / sizeof(nums[0]);
+        for (int i=0 ; i<ArrSize ; i++){
+            for(int j=0 ; i<ArrSize ; j++){
+                if(nums[i] + nums[j]==target){
+                    cout<<i<<","<<j;
+                }
+            }
+
+
+        }
+    
+    system("pause");
+    return 0;
+};
